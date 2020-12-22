4e81b923ab80a7049397b8d088e10a441dd02a1c
joker <2028318192@qq.com>
2020-12-22 11:34:48 +0800

    Initial commit

---
+++ src/com/company/Array.java
@@ -0,0 +1,159 @@
+package com.company;
+
+/**
+ * 功能描述: <br>
+ * <p>〈自定义数组〉</p>
+ *
+ * @author Joker
+ * @ClassName Array
+ * @date 2020/12/22 9:35
+ * @Version 1.0
+ * @ReviseName:
+ * @ReviseTime: 2020/12/22 9:35
+ */
+public class Array<E> {
+    /**
+     * 承载数据的数组
+     */
+    private E[] data;
+    /**
+     * 容量
+     */
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    // 获取数组中的元素个数
+    public int getSize() {
+        return size;
+    }
+
+    // 数组容量
+    public int getCapacity() {
+        return data.length;
+    }
+
+    // 数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 插入最后一个数据
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    // 插入第一个数据
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    // 插入index位置
+        public void add(int index, E e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("AddLast failed. Array is full.");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("AddLast failed. Require index >= 0 and index <= size");
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    // 获取index位置的数据
+    E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        }
+        return data[index];
+    }
+
+    // 修改index的数据
+    void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    // 查找某个位置是否存在元素e
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查询数组中是否存在元素e
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 删除index位置的数据，返回index位置的数据
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+        return ret;
+    }
+
+    // 删除第一个元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    // 删除最后一个元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从数组删除元素
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(",");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Array<Integer> array = new Array<>(20);
+        for (int i = 0; i < 10; i++) {
+            array.addLast(i);
+        }
+        System.out.println(array.toString());
+        array.add(1,100);
+        System.out.println(array);
+        array.add(1,-1);
+        System.out.println(array);
+        array.remove(2);
+        System.out.println(array);
+        array.removeElement(2);
+        System.out.println(array);
+    }
+}
